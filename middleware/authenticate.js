d7ad532bee4d36d62ba729e5e004374e61597af5
Zeeshan Ahmad <cartertaylorcs@gmail.com>
2022-03-26 15:33:23 -0700

    Finalize logic for uploading milestones through CSV

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -53,34 +53,105 @@ const verifyToken = (req, res, next) =>
 
 function retreivePermissions(req, res, next)
 {
+    // Attempt to grab permissions
     let currentUserId = req.userId;
     let permissionsArray = []
-    let sql = mysql.format("SELECT faculty_permissions2.user_id, permissions2.permission_name FROM faculty_permissions2 LEFT JOIN permissions2 ON permissions2.permission_id = faculty_permissions2.permission_id WHERE faculty_permissions2.user_id = ?;", [currentUserId])
-    console.log("Finding permissions for logged in user")
-    // Attempt to grab permissions
-    try
-    {
-        connection.query(sql, function (err, result)
-        {   
-            console.log(result[0])
+    let sql = mysql.format("SELECT faculty_permissions2.user_id, permissions2.permission_name, users2.role FROM faculty_permissions2 LEFT JOIN permissions2 ON permissions2.permission_id = faculty_permissions2.permission_id LEFT JOIN users2 ON users2.user_id = faculty_permissions2.user_id WHERE faculty_permissions2.user_id = ?;", [currentUserId])
+    let userCanUploadNewData = false
 
-            result.forEach(element=>
-                {
-                    console.log(element.permission_name)
-                    if (element.permission_name != "Upload New Data")
-                    {
-                        permissionsArray.push(element.permission_name)
-                    }
-                })
+    let initialSqlCheck = mysql.format("SELECT role FROM users2 WHERE user_id = ?", [currentUserId])
 
-            req.userPermissions = permissionsArray;
+    console.log("Current permissions")
+    console.log(sql)
 
-            next()
-        })
-    }
-    catch{
-            console.log("Failure")
+    try{
         
+        connection.query(initialSqlCheck, function (err, result)
+        {
+            if (result[0].role == 2)
+            {
+                try
+                {
+                    connection.query(sql, function (err, result)
+                    {   
+                        console.log(result[0])
+
+                        result.forEach(element=>
+                            {
+                                console.log(element.permission_name)
+                                
+                                if (element.permission_name == "Upload New Data")
+                                {
+                                    userCanUploadNewData = true;
+                                }
+                                else
+                                {
+                                    console.log("Adding element")
+                                    permissionsArray.push(element.permission_name)
+                                }
+                                console.log(permissionsArray)
+                            })
+
+                        console.log("print permission element")
+                        console.log(permissionsArray)
+
+                        // Store permissions in req
+                        req.userPermissions = permissionsArray
+
+                    })
+                }
+                catch{
+                    req.errorMessage = "Failure"
+                }
+            }
+            // We are an admin, grab list of majors
+            else{
+
+                let adminMajorSql = mysql.format("SELECT permission_name FROM permissions2")
+
+                try{
+                    connection.query(adminMajorSql, function(err, result)
+                    {
+
+                        console.log("Admin matey")
+                        console.log(result)
+
+                        result.forEach(element=>
+                            {
+                                console.log(element.permission_name)
+                                
+                                if (element.permission_name == "Upload New Data")
+                                {
+                                    userCanUploadNewData = true;
+                                }
+                                else
+                                {
+                                    console.log("Adding element")
+                                    permissionsArray.push(element.permission_name)
+                                }
+                            })
+                        
+                        // Store permissions in req
+                        req.userPermissions = permissionsArray
+
+                        next()
+                    })
+                }
+                catch
+                {
+                    req.errorMessage = "Failure"
+                }
+
+
+            }
+        })
+
+        // Attempt to grab permissions
+        
+    }
+    catch
+    {
+        req.errorMessage = "Failure"
     }
 }
 
--- routes/csvUpload.js
+++ routes/csvUpload.js
@@ -23,46 +23,55 @@ router.post('/', function(req, res, next) {
 
 });
 
-router.post('/newStudents', authenticate.verifyToken, function(req, res, next) {
+router.post('/newStudents', authenticate.verifyToken, authenticate.retreivePermissions,function(req, res, next) {
 
     // Get ID of user created this new user from authentication (req.userId) 
     let createdByUserId = req.userId;
     const todaysDate = new Date();
 
+    // Get permissions validated for user by middleware
+    console.log("riggity wrecked")
+    console.log(req.userPermissions )
+    let currentUserPermissions = req.userPermissions;
+    
+
     console.log("Receiving CSV upload for new students")
     console.log(req.body.data)
     let sql = mysql.format("SELECT * FROM users2");
 
+
+    
     let csvRows = req.body.data
 
     let validRow;
     let arrayOfInvalidRows = []
 
-
-
     // Iterate over each row in CSV
-    csvRows.forEach(row =>{
+    csvRows.forEach(row => {
         validRow = true;
         // check each row for a value
         for (col in row)
                 {
-
+                    // Remove extra white space
+                    row[col] = row[col].trim()
+                    
                     if (!checkValidString(row[col]))
-                    {
+                    {   
                         validRow = false;
                         arrayOfInvalidRows.push(row.nau_id)
                     }
 
                 }
-            
-        
-
-        // Only input student if valid row 
-        console.log(arrayOfInvalidRows)
-        if (validRow)
+    
+        // Only input student if valid row and if the student to uploaded matches the users permission
+        if (validRow & currentUserPermissions.includes(row.degree) == 1)
         {
+            console.log("THIS STUDENT MADE IT THROUGH")
+            if (row.nau_id.includes("@"))
+            {
+                row.nau_id = row.nau_id.split("@")[0]
+            }
 
-            row.nau_id = row.nau_id.split("@")[0]
             let gradeDate = getGradeDate(row.graduation_year)
             console.log(gradeDate)
             console.log(todaysDate)
@@ -71,18 +80,20 @@ router.post('/newStudents', authenticate.verifyToken, function(req, res, next) {
                 [row.nau_id, row.first_name, row.last_name, row.degree, gradeDate, todaysDate, createdByUserId]);
 
             console.log(sql)
+
+            connection.query(sql, function (err, result)
+                {   if (err) throw err;
+                    
+                    // console.log(result)
+                    
+                })
         }
 
         // Check that major is in list supported by current user
+        else{
+            arrayOfInvalidRows.push(row.nau_id)
+        }
         
-        connection.query(sql, function (err, result)
-        {   if (err) throw err;
-            
-            console.log(result)
-
-            
-            
-        })
     })
 
     res.json(
@@ -93,27 +104,149 @@ router.post('/newStudents', authenticate.verifyToken, function(req, res, next) {
         }
     )
     
-
-    
-
 });
 
-router.post ('/newMilestones', function(req, res, next) {
+router.post ('/newMilestones',authenticate.verifyToken, authenticate.retreivePermissions, function(req, res) {
     console.log("Receiving CSV upload New milestones")
     console.log(req.body)
 
-    sql = mysql.format("SELECT * FROM students WHERE first_name LIKE ? AND ((?) IS NULL OR degree in (?)) AND school_year >= IF( ? IS NOT NULL,?, 2010 ) AND school_year <= IF( ? IS NOT NULL,?, ? ) LIMIT 10", [
-        searchLetters, joinedMajorFilters, filteredMajors, startYearRange, startYearRange, endYearRange,endYearRange, fourYearsFromNow
-    ]);
+    // Get ID of user created this new user from authentication (req.userId) 
+    let createdByUserId = req.userId;
+
+    // Get permissions validated for user by middleware
+    console.log("riggity wrecked");
+    console.log(req.userPermissions);
+    let currentUserPermissions = req.userPermissions;
+
+    console.log(currentUserPermissions)
+
+    console.log("Receiving CSV upload for new students");
+    console.log(req.body.data);
+    let sql = mysql.format("SELECT * FROM users2");
+
+    let csvRows = req.body.data;
+
+    // Array holds the current selected students milestones to be added
+    let studentMilestoneArray = []
+
+    let validRow;
+    let arrayOfInvalidRows = [];
+    console.log(!csvRows.length < 1)
+        
+    if (!csvRows.length < 1)
+    {
+        console.log("HEY THERE")
+        // Select initial pointer for the student we are gathering milestones for
+        let currentlySelectedStudent = csvRows[0]
+
+        // Iterate over each row in CSV
+        csvRows.forEach((row, index )=> {
+
+            if (row.nau_id.includes("@"))
+            {
+                row.nau_id = row.nau_id.split("@")[0]
+            }
+
+            validRow = true;
+            nextRow = csvRows[index + 1]
+
+            // Save the user of each column 
+
+            // check each column for a value
+            for (col in row)
+            {
+
+                // Remove extra white space
+                row[col] = row[col].trim()
 
 
-    // Send response depending on CSV upload success
-    res.json(
-        {
-            status:"Success",
-            message: "The data was uploaded"
-        }
-    )
+            }
+
+            let milestoneSuccess = undefined
+            
+            // Check for school milestone "school name"
+                // if good, create a milestone array inserstion variable using the column values
+            milestoneSuccess = retreiveMilestone(row, "school", studentMilestoneArray)
+
+            if (milestoneSuccess.properMilestone) 
+            {
+                // Insert the created milestone array into the students global array
+                studentMilestoneArray.push(milestoneSuccess.insertRow)
+            }
+            // Add the student to not inserted
+            else
+            {
+                console.log("not Valid new milestone")
+                arrayOfInvalidRows.push(row.nau_id)
+            }   
+
+            // Check for milestone "job title"
+                // if good, create a milestone array inserstion variable using the column values 
+            milestoneSuccess = retreiveMilestone(row, "normal", studentMilestoneArray)
+
+            if (milestoneSuccess.properMilestone)
+            {
+                // Insert the created milestone array into the students global array
+                studentMilestoneArray.push(milestoneSuccess.insertRow)
+            }
+            // Add the student to not inserted
+            else
+            {
+                console.log("not Valid new milestone")
+                arrayOfInvalidRows.push(row.nau_id)
+            }
+
+            // We have reached the end of tis given user, collect the value of the current row index,
+            if (nextRow == undefined || currentlySelectedStudent.nau_id != nextRow.nau_id )
+            {   
+
+                console.log("Printing out master student milestone list")
+
+                console.log(studentMilestoneArray)
+
+                // Create insert ready variable
+                let values = studentMilestoneArray
+
+
+                // Take the milestone array data and create a SQL query
+                let sql = mysql.format( "INSERT IGNORE INTO milestones_test9 (student_id , milestone_type, milestone_name, milestone_job_title, milestone_description, milestone_location ,date_start, date_end, last_updated) VALUES ?")
+
+                console.log(sql)
+                // Insert each milestones for given student
+                try{
+                    connection.query(sql, [values] , function (err, result)
+                    {   if (err) throw err;
+                        
+                        console.log(result) 
+                        
+                    })
+                }
+                catch{
+                    console.log("Error inserting new milestones")
+                }
+
+                // Iterate to next student since we finished this one
+                currentlySelectedStudent = nextRow
+                
+                // Reset the milestone array to empty for the next student
+                studentMilestoneArray = []
+            }
+
+            if (currentlySelectedStudent == undefined)
+            {
+                res.json(
+                    {
+                        status:"Success",
+                        message: "The data was uploaded",
+                        invalidRows:arrayOfInvalidRows
+                    }
+                )
+            }
+            
+            
+        })
+
+    }
 
 });
 
@@ -130,13 +263,57 @@ function checkValidString(string)
 
 }
 
+// Functions receives CSV row and returns 
+// returns Object
+    // properMilestone: true/false
+    // insertRow: an array with the properly configured milestone values
+// Parameters
+    // milestoneRow: contains the columns of the milestone
+    // typeOfMilestone:"normal" or "school"
+    // studentMilestoneArray: array of the current miletones to be added for student
+function retreiveMilestone(studentRow, typeOfMilestone, studentMilestoneArray)
+{   
+
+    // Variable to hold the individual milestones parameteres
+    let milestone = []
+    
+    const todaysDate = new Date();
+
+    if (typeOfMilestone == "school")
+    {
+        if (checkValidString(studentRow.school_name) & checkValidString(studentRow.nau_id))
+        {
+        // indvidualMilestoneTuple = (studentTableId, milestoneType, schoolName, degree, milestoneDescription, dateStart, dateEnd , currentDate)
+
+            milestone = [studentRow.nau_id, "Education" ,studentRow.school_name, studentRow.area_of_study, studentRow.degree_type, studentRow.education_location, studentRow.education_start_date, studentRow.education_end_date, todaysDate]
+            
+            return {properMilestone:true, insertRow:milestone}  
+        }
+    }
+    else if (typeOfMilestone == "normal")
+    {
+        if (checkValidString(studentRow.milestone_employer) & checkValidString(studentRow.nau_id))
+        {   
+            // indvidualMilestoneTuple = (studentTableId, milestoneType, schoolName, degree, milestoneDescription, dateStart, dateEnd , currentDate)
+
+            milestone = [studentRow.nau_id, "Full Time Job", studentRow.milestone_employer, studentRow.job_title, studentRow.milestone_description, studentRow.milestone_location,studentRow.milestone_start_date, studentRow.milestone_end_date,todaysDate]
+
+            return {properMilestone:true, insertRow:milestone}            
+        }
+    }
+    
+    // Otherwise return false
+    return {properMilestone:false, insertRow:undefined}
+    
+}
+
 function getGradeDate(gradString)
 {
     let seasons = ["Summer", "Fall", "Winter" , "Spring"]
     let splitString = gradString.split(" ")
     let splitYear = splitString[0]
     let splitSeason = splitString[1]
-   
+
     let monthIndex = 4;
     let year = 2022;
     
